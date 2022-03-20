83f5b487f7e5762d960ee56ab2ae910beb66eb93
piotr.sobczyk <piotr.sobczyk@olx.com>
2022-03-20 12:21:11 +0100

    making the file both black and pylint compliant

--- src/dummy_function.py
+++ src/dummy_function.py
@@ -5,9 +5,14 @@ Dummy module
 
 
 def dummy_function(arg_1):
-  return arg_1*2
+    """
+    Multiply by 2
+
+    :param arg_1
+    """
+    return arg_1 * 2
 
 
-if __name__=="main":
-  x=2
-  print(dummy_function(x))
+if __name__ == "main":
+    x = 2  # pylint: disable=C0103
+    print(dummy_function(x))
